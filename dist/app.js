5547ebdbf7e80f81a2e813433a24e93992e45907
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2022-08-09 01:13:25 +0500

    - class basics

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("This is app.js");
+var Department = /** @class */ (function () {
+    function Department(n) {
+        this.employees = [];
+        this.deptName = n;
+    }
+    Department.prototype.describe = function () {
+        console.log("The department is: ", this.deptName);
+    };
+    Department.prototype.addEmployee = function (empName) {
+        this.employees.push(empName);
+    };
+    Department.prototype.printEmployeeInfo = function () {
+        console.log(this.employees, this.employees.length);
+    };
+    return Department;
+}());
+var itDepartment = new Department("IT");
+itDepartment.addEmployee("A. Moiz");
+itDepartment.addEmployee("Lakhani");
+itDepartment.employees[2] = "Test User"; // We should avoid this kind of data addition in real apps.
+itDepartment.describe();
+itDepartment.printEmployeeInfo();
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log("This is app.js");
+class Department {
+  deptName: string;
+  employees: string[] = [];
+
+  constructor(n: string) {
+    this.deptName = n;
+  }
+
+  describe(this: Department) {
+    console.log("The department is: ", this.deptName);
+  }
+
+  addEmployee(empName: string) {
+    this.employees.push(empName);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees, this.employees.length);
+  }
+}
+
+const itDepartment = new Department("IT");
+
+itDepartment.addEmployee("A. Moiz");
+itDepartment.addEmployee("Lakhani")
+
+itDepartment.employees[2] = "Test User"; // We should avoid this kind of data addition in real apps.
+
+itDepartment.describe();
+itDepartment.printEmployeeInfo();
