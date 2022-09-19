6f3b05054b259c7dbf2de41a03993b927eff2457
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2022-09-20 00:16:42 +0500

    - constraints and generic function

--- dist/app.js
+++ dist/app.js
@@ -12,7 +12,15 @@
 function merge(obj1, obj2) {
     return Object.assign(obj1, obj2);
 }
-// const data = merge<{name: string}, {age: number}>({ name: "Abdul Moiz Lakhani" }, { age: 26 });
-var data = merge({ name: "Abdul Moiz Lakhani" }, { age: 26 });
-console.log(data.name, data.age);
+function countAndDescribe(element) {
+    var desc = "No Value!";
+    if (element.length === 1) {
+        desc = "Only 1 Element!!";
+    }
+    else if (element.length > 1) {
+        desc = "This time " + element.length + " elements.";
+    }
+    return [element, desc];
+}
+console.log(countAndDescribe("Test"));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,28 @@
 //   console.log(data);
 // });
 
-function merge<T, U>(obj1: T, obj2: U) {
+function merge<T extends object, U extends object>(obj1: T, obj2: U) {
   return Object.assign(obj1, obj2);
 }
 
 // const data = merge<{name: string}, {age: number}>({ name: "Abdul Moiz Lakhani" }, { age: 26 });
-const data = merge({ name: "Abdul Moiz Lakhani" }, { age: 26 });
+// const data = merge({ name: "Abdul Moiz Lakhani" }, 33); // It will fail because 2nd argument is not an object
+// const data = merge({ name: "Abdul Moiz Lakhani" }, { age: 26 });
 
-console.log(data.name, data.age);
+// console.log(data.name, data.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let desc = "No Value!";
+  if (element.length === 1) {
+    desc = "Only 1 Element!!";
+  } else if (element.length > 1) {
+    desc = "This time " + element.length + " elements.";
+  }
+  return [element, desc];
+}
+
+console.log(countAndDescribe("Test"));
