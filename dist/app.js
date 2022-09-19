3f86c3cb26576a967a58e5087731256686553c81
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2022-09-19 23:58:02 +0500

    - generic function

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,18 @@
 "use strict";
-var names = []; // string[]
-// names[0].split(" ");
-var promise = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        resolve(100);
-    }, 2000);
-});
-promise.then(function (data) {
-    console.log(data);
-});
+// const names: Array<string> = []; // string[]
+// // names[0].split(" ");
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(100);
+//   }, 2000);
+// });
+// promise.then((data) => {
+//   console.log(data);
+// });
+function merge(obj1, obj2) {
+    return Object.assign(obj1, obj2);
+}
+// const data = merge<{name: string}, {age: number}>({ name: "Abdul Moiz Lakhani" }, { age: 26 });
+var data = merge({ name: "Abdul Moiz Lakhani" }, { age: 26 });
+console.log(data.name, data.age);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
-const names: Array<string> = []; // string[]
-// names[0].split(" ");
+// const names: Array<string> = []; // string[]
+// // names[0].split(" ");
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(100);
-  }, 2000);
-});
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(100);
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  console.log(data);
-});
+// promise.then((data) => {
+//   console.log(data);
+// });
+
+function merge<T, U>(obj1: T, obj2: U) {
+  return Object.assign(obj1, obj2);
+}
+
+// const data = merge<{name: string}, {age: number}>({ name: "Abdul Moiz Lakhani" }, { age: 26 });
+const data = merge({ name: "Abdul Moiz Lakhani" }, { age: 26 });
+
+console.log(data.name, data.age);
